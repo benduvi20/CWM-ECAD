269b8cf95d6130ffc4402d5efc4d1362a9e0564b
Cloud User <centos@ip-172-31-2-149.eu-west-2.compute.internal>
2020-06-01 13:20:31 +0000

    trial solutions

--- Ex2/top.v
+++ Ex2/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #2 
-// Student Name:
-// Date: 
+// Student Name: Benjamin Duvieusart
+// Date: 01/06/2020
 //
 //  Description: In this exercise, you need to design a multiplexer, where the  
 //  output acts according to the following truth table:
@@ -21,12 +21,19 @@
 `timescale 1ns / 100ps
 
 module mux(
-    //Todo: define inputs here
+    input a,
+    input b,
+    input sel,
+    output out    
     );
     
     //Todo: define registers and wires here
+    wire T1, T2, selbar;
 
-
-    //Todo: define your logic here                 
+    //Todo: define your logic here
+    not (selbar,sel);
+    and (T1,a,selbar);
+    and (T2,b,sel);
+    or (out,T1,T2);                
       
 endmodule
