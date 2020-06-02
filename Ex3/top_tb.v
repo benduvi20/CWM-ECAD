2a12b978700699c786213c90728645fd7a99a0e9
benduvi20 <66210024+benduvi20@users.noreply.github.com>
2020-06-02 12:24:58 +0300

    Untested solution

--- Ex3/top_tb.v
+++ Ex3/top_tb.v
@@ -15,14 +15,64 @@ module top_tb(
     
 //Todo: Parameters
 
+parameter CLK_PERIOD = 5;
+
 //Todo: Regitsers and wires
+
+reg clk;
+reg err;
+reg rst;
+reg dir;
+reg enable;
+reg [7:0] maxCount;
+
 
 //Todo: Clock generation
 
-//Todo: User logic
-    
-//Todo: Finish test, check for success
+initial begin 
+	clk = 1'b0;
+	forever begin
+		#(CLK_PERIOD / 2) clk = ~clk;
+	end
+end
 
-//Todo: Instantiate counter module
+//Todo: User logic
+initial begin
+	err = 0;
+	rst = 0;	
+	dir = 1;
+	enable = 1;
+
+	#30
+	maxCount <= counter_out
+	dir <= 0; // count down
+	
+	#40
+	if maxCount < counter_out
+		err = 1;
+	enable = 0;
+	#45
+	rst = 1;
+	
+end
+
+initial begin
+	#50
+	if (!err)
+		$display("No errors recorded");
+    
+	$finish; //finish after 50 ticks
+end 
+
+
+counter count(
+	.clk(clk),
+	.rst(rst),
+	.direction(dir),
+	.enable(enable)
+	);
+
+ 
+endmodule 
  
 endmodule 
