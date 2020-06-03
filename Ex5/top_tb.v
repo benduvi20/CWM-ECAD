ee5e74d33e6cc4445220d0a33acee3c71e829804
benduvi20 <noreply@benduvi20.github.com>
2020-06-03 12:34:14 +0000

    solution to Ex5

--- Ex5/top_tb.v
+++ Ex5/top_tb.v
@@ -12,7 +12,7 @@ module top_tb(
 	);
     
 	//Todo: Parameters
-	parameter CLK_PERIOD = 10;
+	parameter CLK_PERIOD = 5;
 
 	//Todo: Regitsers and wires
 	reg clk;
@@ -44,37 +44,37 @@ module top_tb(
 			
 			#CLK_PERIOD
 			
-			if ((RAG_before==3'b000||RAG_before==3'b101||RAG_before==3'b111||RAG_before==3'b011) && (RAG==3'b000||RAG==3'b101||RAG==3'b111|RAG==3'b011))
+			//if ((RAG_before==3'b000||RAG_before==3'b101||RAG_before==3'b111||RAG_before==3'b011) && (RAG==3'b000||RAG==3'b101||RAG==3'b111|RAG==3'b011))
+			if (RAG==3'b000||RAG==3'b101||RAG==3'b111|RAG==3'b011)
 				begin
-					$display("***TEST FAILED!,unable to self start***");
+					$display("***TEST FAILED!***");
 					err=1;
 				end
 			
 			else if ((RAG_before==3'b100) && (RAG!=3'b110))
 				begin
-					$display("***TEST FAILED!,100->110 issue***");
+					$display("***TEST FAILED!***");
 					err=1;
 				end
 
 			else if ((RAG_before==3'b110) && (RAG!=3'b001))
 				begin
-					$display("***TEST FAILED!,110->001 issue***");
+					$display("***TEST FAILED!issue***");
 					err=1;
 				end	
 
 			else if ((RAG_before==3'b001) && (RAG!=3'b010))
 				begin
-					$display("***TEST FAILED!,001->010 issue***");
+					$display("***TEST FAILED!issue***");
 					err=1;
 				end	
 			
          		else if ((RAG_before==3'b010) && (RAG!=3'b001))
 				begin
-					$display("***TEST FAILED!,001->010 issue***");
+					$display("***TEST FAILED!issue***");
 					err=1;
 				end
 
-			// at end, RAG b4 = new value becomes old value
 			RAG_before = RAG;
 
 		end
@@ -82,7 +82,6 @@ module top_tb(
 	
 	//Todo: Finish test, check for success
 	initial begin
-	// 40s, time to complete one loop of testing
         	#50
         	if (err==0)
           		$display("***TEST PASSED! :) ***");
