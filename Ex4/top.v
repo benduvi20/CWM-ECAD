7e83a56c64389854af08ae625a369e380726b916
benduvi20 <noreply@benduvi20.github.com>
2020-06-03 09:26:03 +0000

    solution to Ex4

--- Ex4/top.v
+++ Ex4/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #4 
-// Student Name:
-// Date: 
+// Student Name: Benjamin Duvieusart
+// Date: 03/06/2020
 //
 //  Description: In this exercise, you need to design an electronic dice, following
 //  the diagram provided in the exercises documentation. The dice rolls as long as
@@ -15,4 +15,25 @@
 //
 //  You need to write the whole file.
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
+
+module roll(
+    //ports 
+    input rst,
+    input button,
+    input clk,
+    output reg [2:0] throw );
+
+	//logic
+	always@(posedge clk or posedge rst) begin
+		if(throw == 3'b000 || throw == 3'b111 || throw == 3'b110|| rst)
+			throw = 3'b001; 
+		else 
+			if(button)
+				throw = throw + 1;		
+			else
+				throw = throw; 
+	end
+	
+endmodule
 
--- Ex4/top_tb.v
+++ Ex4/top_tb.v
@@ -1,9 +1,58 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #4 - Electronic Dice
-// Student Name:
-// Date: 
+// Student Name: Benjamin Duvieusart
+// Date: 03/06/2020
 //
 // Description: A testbench module to test Ex4 - Electronic Dice
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
+
+    parameter CLK_PERIOD = 10;
+
+	reg clk, rst, button, err;
+	wire [2:0] throw;
+	
+    initial begin
+    clk = 0;
+    forever
+	if (throw == 3'd7)
+		err = 1
+        #(CLK_PERIOD/2) clk = ~clk;
+    end
+    
+    initial begin
+    	rst = 1;
+    	button = 1;
+    	count = 1;
+    	err = 0;
+    	forever begin
+    	    rst <= 0;
+            #10
+       	    button = 0;
+            #20
+            rst = 1;
+    	end
+    end
+    
+    initial begin
+    	#50
+    	if (err)
+    		$display("***TEST FAILED!***");
+    	else
+    		$display("***TEST PASSED!***");
+    end
+
+
+     //The instantiation of the user's module
+     roll top (
+     .rst (rst),
+     .button (button),
+     .clk (clk),
+     .throw (throw)
+     );
+     
+
+     
+endmodule
