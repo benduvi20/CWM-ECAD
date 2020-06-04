371476d1c9741a19d785d503d00f7d2efd7b1149
benduvi20 <noreply@benduvi20.github.com>
2020-06-04 16:09:11 +0000

    started Ex7

--- Ex7/top.v
+++ Ex7/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #7
-// Student Name:
-// Date: 
+// Student Name: Benjamin Duvieusart
+// Date: 04/06/2020
 //
 //  Description: In this exercise, you need to implement a times table of 0..7x0..7
 //  using a memory.
@@ -12,5 +12,19 @@
 //  outputs:
 //           result[4:0]
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
 
+module times_tables(
+	input clk,
+	input [2:0] a,
+	input [2:0] b,	
+	input read,
+	output [5:0] result
+	);
 
+	wire [5:0] ab;
+	assign ab = {a[2:0],b[2:0]};
+
+	//NEED TO SET UP MEMORY PART
+
+endmodule
--- Ex7/top_tb.v
+++ Ex7/top_tb.v
@@ -1,9 +1,38 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #7 - Times Table
-// Student Name:
-// Date: 
+// Student Name: Benjamin Duvieusart
+// Date: 04/06/2020
 //
 // Description: A testbench module to test Ex7 - Times Table
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns/100ps
+
+module times_table_taster();
+
+	parameter CLK_PERIOD = 10;
+	reg clk;
+	reg read;
+	reg [2:0] a;
+	reg [2:0] b;
+	wire [4:0] result;
+	
+	// clock
+    initial begin
+    clk = 0;
+    forever
+        #(CLK_PERIOD/2) clk = ~clk;
+    end
+	
+
+	//NEED TO TEST
+
+	times_tables top(
+		.clk(clk), 
+		.a(a), 
+		.b(b), 
+		.read(read), 
+		.result(result));
+	
+endmodule
