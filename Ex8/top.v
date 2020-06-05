ab31c256ec91c0621715d0ca7264e9f14706b1c9
benduvi20 <noreply@benduvi20.github.com>
2020-06-05 16:11:50 +0000

    soltuion to Ex8

--- Ex8/top.v
+++ Ex8/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #8
-// Student Name:
-// Date: 
+// Student Name: Benjamin Duvieusart
+// Date: 05/06/202 
 //
 //  Description: In this exercise, you need to implement a times table of 0..7x0..7
 //  using a memory and AXI-4-lite interface.
@@ -12,3 +12,48 @@
 //  outputs:
 //           result[5:0]
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
+
+module times_tables(
+	input clk,
+	input rst,
+	input [2:0] a,
+	input [2:0] b,	
+	input read,
+	output [5:0] result
+	);
+
+	wire [31:0] ab;
+	
+	assign ab = {24'h0,a[2:0],b[2:0],2'h0};
+
+	blk_mem_gen_0 your_instance_name (
+	  .rsta_busy(),          	// output wire rsta_busy
+	  .rstb_busy(),          	// output wire rstb_busy
+
+	  .s_aclk(clk),       		// input wire s_aclk
+	  .s_aresetn(rst),          	// input wire s_aresetn
+
+	  .s_axi_awaddr(0),    		// input wire [31 : 0] s_axi_awaddr
+	  .s_axi_awvalid(0),  		// input wire s_axi_awvalid
+	  .s_axi_awready(),  		// output wire s_axi_awready
+	  .s_axi_wdata(0),      	// input wire [31 : 0] s_axi_wdata
+	  .s_axi_wstrb(0),      	// input wire [3 : 0] s_axi_wstrb
+	  .s_axi_wvalid(0),    		// input wire s_axi_wvalid
+	  .s_axi_wready(),    		// output wire s_axi_wready
+	  .s_axi_bresp(),      		// output wire [1 : 0] s_axi_bresp
+	  .s_axi_bvalid(),    		// output wire s_axi_bvalid
+	  .s_axi_bready(0),    		// input wire s_axi_bready
+
+	  .s_axi_araddr(ab),    	// input wire [31 : 0] s_axi_araddr
+	  .s_axi_arvalid(read),  	// input wire s_axi_arvalid
+	  .s_axi_arready(),  		// output wire s_axi_arready
+	  .s_axi_rdata(result),      	// output wire [31 : 0] s_axi_rdata
+	  .s_axi_rresp(),      		// output wire [1 : 0] s_axi_rresp
+	  .s_axi_rvalid(),    		// output wire s_axi_rvalid
+	  .s_axi_rready(read)    	// input wire s_axi_rready
+	);
+
+
+
+endmodule
--- Ex8/top_tb.v
+++ Ex8/top_tb.v
@@ -1,9 +1,85 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #8 - Times Table using AXI interface
-// Student Name:
-// Date: 
+// Student Name: Benjamin Duvieusart
+// Date: 05/06/202
 //
 // Description: A testbench module to test Ex8 - Times Table using AXI4-Lite
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
+// Test bench for Exercise #7 - Times Table
+// Student Name: Benjamin Duvieusart
+// Date: 04/06/2020
+//
+// Description: A testbench module to test Ex7 - Times Table
+// You need to write the whole file
+//////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
+
+
+module top_tb(
+	);
+    
+	//Todo: Parameters
+	parameter CLK_PERIOD = 10;
+
+	//Todo: Regitsers and wires
+	reg clk;
+	reg rst;
+	reg err;
+	reg [2:0] a;	
+	reg [2:0] b;
+	reg read;
+	wire [5:0] result;
+	
+	//Todo: Clock generation
+	initial	begin
+  		clk = 1'b0;
+  		forever
+  			#(CLK_PERIOD/2) clk=~clk;
+  	end
+
+	//Todo: User logic
+	initial begin
+		a=0;
+		b=0;		
+		rst = 1;
+		read = 1;
+		err=0;
+
+		forever begin
+			
+			#CLK_PERIOD
+			if (read && a*b!=result)
+				begin
+					$display("***TEST FAILED!,wrong calculation***");
+					err=1;
+				end
+			
+			a = a+1;
+			if (a==0)
+				b = b+1;
+		end
+	end
+	
+	//Todo: Finish test, check for success
+	initial begin
+        	#640
+        	if (err==0)
+          		$display("***TEST PASSED! :) ***");
+		$finish;
+	end
+
+
+	//Todo: Instantiate times tables module
+	times_tables top (
+		.clk (clk),
+		.rst (rst),
+		.a (a),
+		.b (b),
+		.read (read),
+		.result (result)
+	);
+	
+endmodule  
